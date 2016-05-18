8ab11532f9307e4e141c173b60b940c94f77e172
michaeldelli-gatti <michaelddg@gmail.com>
2016-05-18 14:14:34 -0400

    assignment completed

---
+++ src/com/theironyard/Customer.java
@@ -0,0 +1,53 @@
+package com.theironyard;
+
+import static com.theironyard.Main.scanner;
+
+/**
+ * Created by michaeldelli-gatti on 5/18/16.
+ */
+public class Customer {
+    String name;
+    int amtInt;
+    int bal;
+
+    public void greeting() {
+        System.out.println("What is your name?");
+
+        name = scanner.nextLine();
+
+        System.out.println("Hello " + name + ". How may I help you today?");
+        System.out.println("Press 1 to check your balance.");
+        System.out.println("Press 2 to withdraw funds.");
+        System.out.println("Press 3 to cancel");
+    }
+    public void input() throws Exception {
+        String selection = scanner.nextLine();
+
+        bal = 100;
+
+        if (selection.equals("1")){
+            System.out.println("Your balance is $" + bal);
+        }
+        else if (selection.equals("2")) {
+            System.out.println("How much would you like to withdraw?");
+            String amt = scanner.nextLine();
+            int amtInt = Integer.valueOf(amt);
+            if (amtInt > 100) {
+                throw new Exception ("You and I both know you don't have that much money.");
+            }
+            else if (amtInt > 0) {
+                System.out.println("you have withdrawn $" + amt + ". Please take your money. Your new balance is $" + (bal - amtInt) + ".");
+            }
+            else {
+                System.out.println("We don't do deposits here.");
+            }
+        }
+        else if (selection.equals("3")) {
+            System.out.println("Thank you " + name + " please come again.");
+        }
+        else {
+            throw new Exception("Invalid Selection");
+        }
+    }
+
+}
--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -8,40 +8,12 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome!");
-        System.out.println("What is your name?");
 
-        String name = scanner.nextLine();
+        Customer customer = new Customer();
 
-        System.out.println("Hello " + name + ". How may I help you today?");
-        System.out.println("Press 1 to check your balance.");
-        System.out.println("Press 2 to withdraw funds.");
-        System.out.println("Press 3 to cancel");
+        customer.greeting();
+        customer.input();
 
-        String selection = scanner.nextLine();
-
-        if (selection.equals("1")){
-            System.out.println("Your balance is $100");
-        }
-        else if (selection.equals("2")) {
-            System.out.println("How much would you like to withdraw?");
-            String amt = scanner.nextLine();
-            int amtInt = Integer.valueOf(amt);
-            if (amtInt > 100) {
-                throw new Exception ("You and I both know you don't have that much money.");
-            }
-            else if (amtInt > 0) {
-                System.out.println("you have withdrawn " + amt + " dollars. Please take your money.");
-            }
-            else {
-                System.out.println("Did you mean to select deposit?");
-            }
-        }
-        else if (selection.equals("3")) {
-            System.out.println("Thank you and please come again.");
-        }
-        else {
-            throw new Exception("Invalid Selection");
-        }
 
 
     }
